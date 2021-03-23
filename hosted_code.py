207177cd9c3806292411206b57b4c92f6af905cf
TomPMcGuire37 <tompmcguire90@gmail.com>
2021-03-22 21:11:28 -0400

    Added notebook
    trying a different approach

--- hosted_code.py
+++ hosted_code.py
@@ -5,14 +5,17 @@ import seaborn as sns
 
 data=pd.read_csv('C:/Users/Tom/git/pythonProject/absenteeism/Absenteeism_at_work.csv', index_col=False, sep=';')
 
-print(data.head())
-print(data.info())
-print(data.describe())
-print(data.isnull().sum())
+#print(data.head())
+#print(data.info())
+#print(data.describe())
+#print(data.isnull().sum())
 
 """sns.set_theme(style="darkgrid")
-sns.histplot(x=['Reason for absence'], data=data)
+sns.scatterplot(data['Age'])
 plt.show()"""
 
-_ = plt.hist(data['Reason for absence'], bins='auto')
+age_hist = plt.hist(data['Age'], bins='auto')
+plt.show()
+
+reason_hist = plt.hist(data['Reason for absence'], bins='auto')
 plt.show()
